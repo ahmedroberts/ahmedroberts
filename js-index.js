2c9fbe25ec5dc4338b2657b372c2a5a62b028ff0
Ahmed Roberts <ahmedroberts@gmail.com>
2022-10-30 23:24:44 -0400

    Prep 2022 for the 2023 run

---
+++ js-index.js
@@ -0,0 +1,41 @@
+/*
+    Ahmed O. Roberts
+    2022-10-30
+    JavaScript Endeavors
+    This will be me making sure I stay Familiar with JavaScript.
+*/
+
+// Hello World.
+console.log('hello world')
+
+
+// An array of objects.
+// https://emojipedia.org
+const blackSupers = [
+  {
+    id: 1,
+    heroName: 'Icon',
+    realName: 'Augustus Freeman IV',
+    emoji: '🐂'
+  },
+  {
+    id: 2,
+    heroName: 'Blue Marvel',
+    realName: 'Adam Brashear',
+    emoji: '🐘' 
+  },
+  {
+    id: 3,
+    heroName: 'Green Lantern',
+    realName: 'John Stewart',
+    emoji: '🦏'
+  },
+  {
+    id: 4,
+    heroName: 'Black Panther',
+    realName: "T'Challa son of T'Chaka",
+    emoji: '🦛' 
+  }
+];
+
+console.log(blackSupers);
